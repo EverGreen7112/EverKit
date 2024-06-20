56134048354b85f6a317cc1513272cade3a261dc
ori sharim <ori.sharim@gmail.com>
2024-06-20 14:20:00 +0300

    added documentation to cancoder

--- EverKit/src/main/java/frc/robot/EverKit/Implementations/Encoders/EverCANCoder.java
+++ EverKit/src/main/java/frc/robot/EverKit/Implementations/Encoders/EverCANCoder.java
@@ -11,6 +11,9 @@ public class EverCANCoder implements EverAbsEncoder{
     private double m_posFactor;
     private double m_velFactor;
 
+    /**
+     * @param id - id of encoder
+     */
     public EverCANCoder(int id){
         m_encoder = new CANcoder(id);
         m_encoder.setPosition(0);
@@ -19,21 +22,33 @@ public class EverCANCoder implements EverAbsEncoder{
         m_velFactor = 1;
     }
 
+    /**
+     * returns absolute position of encoder in units of rotations unless conversion factor was changed. 
+     */
     @Override
     public double getAbsPos() {
         return m_encoder.getAbsolutePosition().getValue() * m_posFactor;
     }
 
+    /**
+     * returns relative position of encoder in units of rotations unless conversion factor was changed. 
+     */
     @Override
     public double getPos() {
        return m_encoder.getPosition().getValue() * m_posFactor;
     }
 
+    /**
+     * @param pos - new position of encoder in units of rotations unless conversion factor was changed. .
+     */
     @Override
     public void setPos(double pos) {
-        m_encoder.setPosition(pos);
+        m_encoder.setPosition(pos / m_posFactor);
     }
 
+    /**
+     * returns velocity in units of rotations per second unless conversion factor was changed. 
+     */
     @Override
     public double getVel() {
         return m_encoder.getVelocity().getValue() * m_velFactor;
@@ -41,20 +56,26 @@ public class EverCANCoder implements EverAbsEncoder{
 
     @Override
     public double getOffset() {
-        return m_configs.MagnetOffset;
+        return m_configs.MagnetOffset * m_posFactor;
     }
 
     @Override
     public void setOffset(double offset) {
-        m_configs.withMagnetOffset(offset);
-        m_encoder.getConfigurator().apply(m_configs);
+        m_configs.withMagnetOffset(1 * m_posFactor - offset / m_posFactor);
+        m_encoder.getConfigurator().apply(m_configs);        
     }
 
+    /**
+     * convert units of measure of the encoder from rotations.
+     */
     @Override
     public void setPosConversionFactor(double factor) {
         m_posFactor = factor;
     }
 
+    /**
+     * convert units of measure of the encoder from rotations per second.
+     */
     @Override
     public void setVelConversionFactor(double factor) {
         m_velFactor = factor;
